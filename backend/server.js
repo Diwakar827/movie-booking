aebcee28838ab3d3a099222f5093fe7357d310e0
Diwakar <diwakarchauhaun827@gmail.com>
2023-09-22 19:16:55 +0530

    corss change

--- backend/server.js
+++ backend/server.js
@@ -12,8 +12,20 @@ dotenv.config();
 const app = express();
 
 // middlewares
-app.use(cors());
-app.use(express.json());
+
+
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+    res.setHeader("credentials","true");
+    next();
+  })
+  
+  app.use(cors());
+  
+  app.use(express.json());
+
 app.use("/user", userRouter);
 app.use("/admin", adminRouter);
 app.use("/movie", movieRouter);
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,7 +7,7 @@ import axios from "axios";
 import { Provider } from "react-redux";
 import { store } from "./store";
 const root = ReactDOM.createRoot(document.getElementById("root"));
-axios.defaults.baseURL = "http://localhost:5000";
+axios.defaults.baseURL ="https://movie-booking-six-alpha.vercel.app/";  // insted of "http://localhost:5000" this for deplpyment
 root.render(
   <React.StrictMode>
     <BrowserRouter>
